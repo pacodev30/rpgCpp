49becec1d4e329f624f7827d6a13eb80d67ecb0c
pacodev30 <pacofustier@gmail.com>
2025-01-09 18:32:47 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -24,3 +24,34 @@ int main()
     return 0;
 }
 
+/*
+Welcome to Esteban, a valiant warrior :)
+Welcome to Hulk, a valiant warrior :)
+Welcome to Stella a wise mage ;)
+
+--- GAMERS ---
+Esteban| HP(100) | Warrior(Saber)
+Hulk| HP(100) | Warrior(Hammer)
+Stella| HP(100) | Mana(40) | Mage(Fireball)
+-------------
+
+-> Hulk attack Stella
+Stella(60)
+-> Esteban attack Hulk
+Hulk(70)
+-> Esteban attack Hulk
+Hulk(40)
+-> Esteban attack Hulk
+Hulk(10)
+-> Esteban attack Hulk
+Hulk(0)
+
+Hulk is dead :(
+ -- End of the game ---
+
+<- Destruct Esteban
+<- Destruct Hulk
+<- Destruct Stella
+
+Le processus s'est terminé avec le code : 0.
+*/
